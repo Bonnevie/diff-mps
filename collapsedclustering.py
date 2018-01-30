Corrected fatal sign bug. Added functionality (e.g. norms). New experiment.

collapsedclustering.py
@@ -70,7 +70,7 @@ class CollapsedMixture:
         return tf.map_fn(lambda x: tf.map_fn(logpx, x, dtype=dtype), Zi, dtype=dtype) #<--
 
     @tfmethod(1)
-    def populatetensor(self, X):
+    def populatetensor(self, X, sess=None):
         sess = tf.get_default_session()
         shape = self.N*(self.K,)
         size = self.K**self.N
@@ -106,7 +106,7 @@ class CollapsedMultipartite(CollapsedMixture):
         return logp_cond
 
     @tfmethod(1)
-    def populatetensor(self, X):
+    def populatetensor(self, X, normalize=True):
         sess = tf.get_default_session()
         shapes = [N*(K, ) for N, K in zip(self.Ns, self.Ks)]
         shape = tuple(chain(*shapes))
@@ -123,8 +123,10 @@ class CollapsedMultipartite(CollapsedMixture):
             """Compute softmax values for each sets of scores in x."""
             e_x = np.exp(x - np.max(x))
             return e_x / e_x.sum()
-        return np.reshape(softmax(Zstar_vec), shape)
-
+        if normalize:
+            return np.reshape(softmax(Zstar_vec), shape)
+        else:
+            return np.reshape(Zstar_vec, shape)
 
 class CollapsedStochasticBlock(CollapsedMixture):
     def __init__(self, N, K, alpha=1., a=1., b=1.):

rankinfluence.py
@@ -7,63 +7,32 @@ dtype = 'float64'
 
 import tqdm
 
-from collapsedclustering import CollapsedStochasticBlock
-from tensornets import MPS, Canonical, symmetrynorm, CayleyOrthogonal
+from collapsedclustering import CollapsedStochasticBlock, KLcorrectedBound
+import tensornets as tn
 from edward.models import MultivariateNormalTriL, Dirichlet, WishartCholesky, ParamMixture
 
+name = 'marginalentropy'
+coretype = 'Tcanon' #'Tperm', 'Tcanon', ''
 N = 10
 K = 3
-maxranks = [1,20]
-nsamples=1000
-steps = 20000
-runs = 3
+maxranks = [1, 3, 9]#[1,6,9,12,15,18,21,24]
+nsamples=500
+steps = 500
+runs = 1
+version = 1
+rate = 0.001
 
-folder = 'testN{}K{}R{}'.format(N, K, '-'.join(str(rank) for rank in maxranks))
+folder = name + '{}V{}N{}K{}S{}R{}'.format(coretype,version, N, K, nsamples, '-'.join(str(rank) for rank in maxranks))
 #cap core rank at R
 
 timeit = False
+calculate_true = True
 
 np.random.seed(1)
 
 #5-22
-#10-54
-
-
-Akarate = np.array([   [ 0.,  0.,  1.,  1.,  1.,  1.,  1.,  1.,  1.,  0.,  1.,  1.,  1., 1.,  0.,  0.,  0.,  1.,  0.,  1.,  0.,  1.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  1.,  0.,  0.],
-        [ 0.,  0.,  1.,  1.,  0.,  0.,  0.,  1.,  0.,  0.,  0.,  0.,  0., 1.,  0.,  0.,  0.,  1.,  0.,  1.,  0.,  1.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  1.,  0.,  0.,  0.],
-        [ 1.,  1.,  0.,  1.,  0.,  0.,  0.,  1.,  1.,  1.,  0.,  0.,  0., 1.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  1.,  1.,  0.,  0.,  0.,  1.,  0.],
-        [ 1.,  1.,  1.,  0.,  0.,  0.,  0.,  1.,  0.,  0.,  0.,  0.,  1., 1.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.],
-        [ 1.,  0.,  0.,  0.,  0.,  0.,  1.,  0.,  0.,  0.,  1.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.],
-        [ 1.,  0.,  0.,  0.,  0.,  0.,  1.,  0.,  0.,  0.,  1.,  0.,  0., 0.,  0.,  0.,  1.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.],
-        [ 1.,  0.,  0.,  0.,  1.,  1.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  1.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.],
-        [ 1.,  1.,  1.,  1.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.],
-        [ 1.,  0.,  1.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  1.,  0.,  1.,  1.],
-        [ 0.,  0.,  1.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  1.],
-        [ 1.,  0.,  0.,  0.,  1.,  1.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.],
-        [ 1.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.],
-        [ 1.,  0.,  0.,  1.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.],
-        [ 1.,  1.,  1.,  1.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  1.],
-        [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  1.,  1.],
-        [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  1.,  1.],
-        [ 0.,  0.,  0.,  0.,  0.,  1.,  1.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.],
-        [ 1.,  1.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.],
-        [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  1.,  1.],
-        [ 1.,  1.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  1.],
-        [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  1.,  1.],
-        [ 1.,  1.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.],
-        [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  1.,  1.],
-        [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  1., 0.,  1.,  0.,  1.,  0.,  0.,  1.,  1.],
-        [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  1., 0.,  1.,  0.,  0.,  0.,  1.,  0.,  0.],
-        [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  1.,  1.,  0., 0.,  0.,  0.,  0.,  0.,  1.,  0.,  0.],
-        [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  1.,  0.,  0.,  0.,  1.],
-        [ 0.,  0.,  1.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  1.,  1.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  1.],
-        [ 0.,  0.,  1.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  1.,  0.,  1.],
-        [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  1.,  0.,  0., 1.,  0.,  0.,  0.,  0.,  0.,  1.,  1.],
-        [ 0.,  1.,  0.,  0.,  0.,  0.,  0.,  0.,  1.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  1.,  1.],
-        [ 1.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  1.,  1., 0.,  0.,  1.,  0.,  0.,  0.,  1.,  1.],
-        [ 0.,  0.,  1.,  0.,  0.,  0.,  0.,  0.,  1.,  0.,  0.,  0.,  0., 0.,  1.,  1.,  0.,  0.,  1.,  0.,  1.,  0.,  1.,  1.,  0.,  0., 0.,  0.,  0.,  1.,  1.,  1.,  0.,  1.],
-        [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  1.,  1.,  0.,  0.,  0., 1.,  1.,  1.,  0.,  0.,  1.,  1.,  1.,  0.,  1.,  1.,  0.,  0., 1.,  1.,  1.,  1.,  1.,  1.,  1.,  0.]])
-X = Akarate[:N,:N]
+X = np.random.randn(*(N, N))
+X =(1.-np.eye(N))*(np.tril(X) + np.tril(X).T)
 tf.reset_default_graph()
 
 cores = {}
@@ -72,56 +41,100 @@ softelbo = {}
 opt = {}
 step = {}
 equalize_ops = []
-nancheck = False
+init_opt = {}
 with tf.name_scope("model"):
     p = CollapsedStochasticBlock(N, K, alpha=100, a=10, b=10)
+    Z = tf.Variable(tf.random_normal((10, N, K), dtype=dtype))
+
+    bounds = -tf.reduce_mean([KLcorrectedBound(p, X, [z]).bound for z in tf.unstack(Z, axis=0)])
+    mode_opt = tf.contrib.opt.ScipyOptimizerInterface(bounds, var_list=[Z])
+    
     Xt = tf.constant(X)
     print("building models...")
     for R in tqdm.tqdm(maxranks):
         with tf.name_scope("rank"+str(R)):
-            ranks = tuple(min(K**min(r, N-r), R) for r in range(N+1))
-            cores[R] = Canonical(N, K, ranks, orthogonalstyle=CayleyOrthogonal)
-            q[R] = MPS(N, K, ranks, cores=cores[R])
+
+            if coretype is 'Tcanon':
+                ranks = tuple(min(K**min(r, N-r), R) for r in range(N+1))#(1,) + tuple() + (1,)
+                cores[R] = tn.Canonical(N, K, ranks, orthogonalstyle=tn.CayleyOrthogonal)
+            elif coretype is 'Tperm':
+                ranks = tuple(min(K**min(r, N-r), R) for r in range(N+1))
+                repranks = (1,)+tuple(min((2)**min(r, N-r-2)*K, R) for r in range(N-1))+(1,)
+                cores[R] = tn.PermutationCore_augmented(N, K, repranks, ranks)
+            else:
+                ranks = tuple(min(K**min(r, N-r), R) for r in range(N+1))
+                cores[R] = tn.Core(N, K, ranks)
+            q[R] = tn.MPS(N, K, ranks, cores=cores[R])
             softelbo[R] = tf.reduce_mean(q[R].elbo(lambda sample: p.batch_logp(sample, Xt), nsamples=nsamples, fold=False))
+            #softelbo[R] = tf.reduce_mean(q[R].elbo(lambda sample: 0., nsamples=nsamples, fold=False))
+            mode_loss = -(q[R].marginalentropy())
+            #mode_loss = tf.convert_to_tensor(np.array(0.).astype('float64'))
+            mode_loss = tf.reduce_sum(tn.norm_rank1(q[R], tf.nn.softmax(Z))) 
+            init_opt[R] = tf.contrib.opt.ScipyOptimizerInterface(mode_loss)
+            #init_opt[R] = tf.contrib.opt.ScipyOptimizerInterface()
+            opt[R] = tf.train.AdamOptimizer(learning_rate=rate)
+            step[R] = opt[R].minimize(-softelbo[R], var_list=cores[R].params())
+            tf.summary.scalar('ELBO', softelbo[R])
 
-            opt[R] = tf.train.AdamOptimizer()
-            step[R] = opt[R].minimize(softelbo[R])
-            tf.summary.scalar('ELBO', -softelbo[R])
 
-    train = tf.group(*step.values())
-    init = tf.global_variables_initializer()
-    summaries = tf.summary.merge_all()
+
+
+
     if timeit:
         run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
         run_metadata = tf.RunMetadata()
 
-    #tf.get_default_graph().finalize()
-    with tf.name_scope("optimization"):
-        sess = tf.Session()
-        if nancheck:
-            nonan = True
-            sess.run(init)
-            g = opt[1].compute_gradients(softelbo[1], cores[1].params())
-            update = opt[1].apply_gradients(g)
+    train = tf.group(*step.values())
+    summaries = tf.summary.merge_all()
 
-            nonan_op = tf.logical_not(tf.reduce_any([tf.is_nan(gi[0]) for gi in g if gi[0] is not None]))
-            nonan_op = tf.Print(nonan_op, [softelbo[1]])
-            nonan = True
-            while nonan:
-                sess.run(update)
-                nonan = sess.run(nonan_op)
-        else:
-            print("Starting optimization.")
-            for run in tqdm.tqdm(range(runs), desc="Run", total=5):
-                writer = tf.summary.FileWriter('./train/' + folder + 'run' + str(run), sess.graph)
-                sess.run(init)
-                for it in tqdm.tqdm(range(steps), desc='Optimization step', total=steps, leave=False):
-                    if timeit:
-                        _, it_summary = sess.run([train, summaries],
-                                                 options=run_options,
-                                                 run_metadata=run_metadata)
-                    else:
-                        _, it_summary = sess.run([train, summaries])
-                    writer.add_summary(it_summary, it)
-                    if timeit:
-                        writer.add_run_metadata(run_metadata, "trace{}".format(it))
+    sess = tf.Session()
+    with tf.name_scope("truemodel"):
+        if calculate_true:
+            with sess.as_default():
+                ptensor = p.populatetensor(X)
+                pcore, pmps = tn.full2TT(np.sqrt(ptensor))
+                true_loss_op = tf.reduce_mean(pmps.elbo(lambda sample: p.batch_logp(sample, Xt), nsamples=nsamples, fold=False))
+
+    init = tf.global_variables_initializer()
+    sess.run(init)
+
+    if calculate_true:
+        true_loss = sess.run(true_loss_op)
+    #tf.get_default_graph().finalize()
+    with tf.name_scope("optimization"):    
+        Q = {}
+        print("Starting optimization.")
+        for run in tqdm.tqdm(range(runs), desc="Run", total=runs):
+            writer = tf.summary.FileWriter('./train/' + folder + 'run' + str(run), sess.graph)
+            sess.run(init)
+            mode_opt.minimize(sess)
+            print('Initializing...')
+            for R in tqdm.tqdm(maxranks):
+                init_opt[R].minimize(sess)
+            
+            print('Starting gradient ascent...')
+            for it in tqdm.tqdm(range(steps), desc='Optimization step', total=steps, leave=False):
+                if timeit:
+                    _, it_summary = sess.run([train, summaries],
+                                                options=run_options,
+                                                run_metadata=run_metadata)
+                else:
+                    _, it_summary = sess.run([train, summaries])
+                writer.add_summary(it_summary, it)
+                if timeit:
+                    writer.add_run_metadata(run_metadata, "trace{}".format(it))
+            Q[run] = q
+
+if False:
+    if nancheck:
+        nonan = True
+        sess.run(init)
+        g = opt[1].compute_gradients(softelbo[1], cores[1].params())
+        update = opt[1].apply_gradients(g)
+
+        nonan_op = tf.logical_not(tf.reduce_any([tf.is_nan(gi[0]) for gi in g if gi[0] is not None]))
+        nonan_op = tf.Print(nonan_op, [softelbo[1]])
+        nonan = True
+        while nonan:
+            sess.run(update)
+            nonan = sess.run(nonan_op)