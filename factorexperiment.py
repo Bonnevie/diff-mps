Fixed sign flips for KL in optrank and RELAX objective in factoredexperiments. Normalized baseline (ptensor_p) in optrank.

factorexperiment.py
@@ -16,13 +16,13 @@ from itertools import product
 timeit = False #log compute time of every op for Tensorboard visualization (costly)
 calculate_true = True #calculate true tensor and MPS (WARNING: can cause OOM for N>>14)
 do_anneal = False #do entropy annealing
-name = 'relax' 
+name = 'varreduc' 
 datatype = 'random' #'random', 'blocked', or 'karate'
-version = 2
+version = 1
 N = 7 #number of vertices in graph
-Ntest = 0 #number of edges to use for testing
+Ntest = 5 #number of edges to use for testing
 K = 3 #number of communities to look for
-nsamples=1000 #number of gradient samples per iteration
+nsamples=100 #number of gradient samples per iteration
 steps = 10000 #number of optimization iterations
 decay_steps = 600 #number of steps between learning rate decay
 anneal_decay_steps = 100 #number of steps beteween anneal decay
@@ -36,9 +36,9 @@ folder = name + 'D{}V{}N{}K{}S{}'.format(datatype,version, N, K, nsamples)
 
 #factors variations to run experiments over
 copies = 1
-coretypes = [''] #types of cores to try 
+coretypes = ['','canon'] #types of cores to try 
 #Options are: '' for ordinary cores, canon' for canonical, and 'perm' for permutation-free
-inittypes = ['random'] #whether to run an initialization routine
+inittypes = ['random','expectation'] #whether to run an initialization routine
 #Options are: 'random' for random values, 'entropy' for maximum marginal entropy, 'rank1' for minimum norm to tensor mixture of 10 modes the model,
 #'expectation' for maximum expectation of the the previous tensor mixture under q.
 maxranks = [9]
@@ -150,7 +150,7 @@ with tf.name_scope("model"):
     Xt = tf.constant(X)
     print("building models...")
     for config in tqdm.tqdm(all_config, total=config_count):
-        coretype, init, opt_name, objective, do_project, decay_rate, anneal_decay_rate, R = config
+        coretype, init, opt_name, objective_name, do_project, decay_rate, anneal_decay_rate, R = config
         if short_key:
             config_name = ''.join([''.join([key, str(value)]) for key, value, active in zip(factor_code, config, active_factors) if active])
         else:
@@ -181,15 +181,16 @@ with tf.name_scope("model"):
                     anneal_invtemp = 1.
                 q[configc] = tn.MPS(N, K, ranks, cores=cores[configc])
                 cvweight = 1.
-                if objective is '':
+                if objective_name is '':
                     objective, elbo, loss, entropy, marginalentropy, marginalcv = (q[configc].elbo(lambda sample: p.batch_logp(sample, Xt, observed=mask), nsamples=nsamples, fold=False, report=True, cvweight=cvweight, invtemp=anneal_invtemp))
-                elif objective is 'modes':
+                elif objective_name is 'modes':
                     objective, elbo, loss, entropy, marginalentropy, marginalcv = (q[configc].elbowithmodes(lambda sample: p.batch_logp(sample, Xt, observed=mask), anchors, nsamples=nsamples, fold=False, report=True, cvweight=cvweight, invtemp=anneal_invtemp))
-                elif objective is 'relax':
-                    relax_params = q[configc].buildcontrol(lambda sample: tf.reduce_mean(p.batch_logp(sample, Xt, observed=mask) - tf.log(1e-16+q[configc].batch_contraction(sample))), nsamples=nsamples, fold=False)
+                elif objective_name is 'relax':
+                    relax_params = q[configc].buildcontrol(lambda sample: -tf.reduce_mean(p.batch_logp(sample, Xt, observed=mask) - tf.log(1e-16+q[configc].batch_contraction(sample))), nsamples=nsamples, fold=False)
                     objective, elbo, loss, entropy, marginalentropy, marginalcv = (q[configc].elbo(lambda sample: p.batch_logp(sample, Xt, observed=mask), nsamples=nsamples, differentiable=False, fold=False, report=True, cvweight=cvweight, invtemp=anneal_invtemp))
                     grads, vargrads = RELAX(*relax_params, cores[configc].params(), var_params=q[configc].var_params()) 
-                elif objective is 'relax_neural':
+                    grads = grads + vargrads
+                elif objective_name is 'relax_neural':
                     pass
 
                 #objective, elbo, loss, entropy, marginalentropy, marginalcv = (q[configc].elbowithmodes(lambda sample: p.batch_logp(sample, Xt, observed=mask), anchors, nsamples=nsamples, fold=False, report=True, cvweight=cvweight, invtemp=anneal_invtemp))
@@ -211,7 +212,7 @@ with tf.name_scope("model"):
                 elif init is 'expectation':
                     mode_loss = -tf.reduce_sum(tf.log(q[configc].batch_contraction(tf.nn.softmax(Z))))
 
-                init_ops[configc] = q[configc].set_temperature(temperature)
+                init_ops[configc] = tf.group(q[configc].set_temperature(temperature), q[configc].set_nu(1.))
                 init_opt[configc] = tf.contrib.opt.ScipyOptimizerInterface(mode_loss, var_list=cores[configc].params(),method='CG')
                 
                 if do_project:
@@ -220,7 +221,7 @@ with tf.name_scope("model"):
                     
                 #build optimizers
                 decayed_rate = tf.train.exponential_decay(tf.cast(rate, dtype), global_step, decay_steps, decay_rate, staircase=True)
-                if objective is not 'relax' and objective is not 'relax_neural':
+                if objective_name is not 'relax' and objective is not 'relax_neural':
                     grads = zip(tf.gradients(-softobj[configc], cores[configc].params()), cores[configc].params())
                 
                 if opt_name is 'Adam':
@@ -238,7 +239,9 @@ with tf.name_scope("model"):
                                 tf.summary.scalar('pred', tf.reduce_mean(pred)),
                                 tf.summary.scalar('logp', tf.reduce_mean(loss)),
                                 tf.summary.scalar('entropy', tf.reduce_mean(entropy)),
-                                tf.summary.scalar('marginalentropy', tf.reduce_mean(marginalentropy))]
+                                tf.summary.scalar('marginalentropy', tf.reduce_mean(marginalentropy)),
+                                tf.summary.scalar('temperature',tf.reduce_mean(q[configc].temperatures)),
+                                tf.summary.scalar('nu',q[configc].nu)]
     if timeit:
         run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
         run_metadata = tf.RunMetadata()

optrank.py
@@ -17,7 +17,7 @@ from itertools import product
 timeit = False #log compute time of every op for Tensorboard visualization (costly)
 calculate_true = False #calculate true tensor and MPS (WARNING: can cause OOM for N>>14)
 do_anneal = False #do entropy annealing
-name = 'lowrank' 
+name = 'normed' 
 version = 3
 N = 7 #number of vertices in graph
 Ntest = 5 #number of edges to use for testing
@@ -135,11 +135,11 @@ with tf.name_scope("model"):
                 logZ[copy] = np.logaddexp.reduce(logptensor[copy].ravel())
                 ptensor[copy] = np.exp(logptensor[copy] - logZ[copy])
                 for rank in tqdm.tqdm(maxranks, total=len(maxranks)):
-                    pcore, pmps = tn.full2TT(np.sqrt(ptensor[copy]), rank)
+                    pcore, pmps = tn.full2TT(np.sqrt(ptensor[copy]), rank, normalized=True)
                     pred_p = sess.run(tf.reduce_sum(pmps.batch_contraction(all_anchors)*(p.batch_logp(all_anchors,X[copy],observed=predictionmask))))
                     elbo_p = sess.run(tf.reduce_sum(pmps.batch_contraction(all_anchors)*(p.batch_logp(all_anchors,X[copy],observed=mask)-tf.log(1e-16+pmps.batch_contraction(all_anchors)))))
                     ptensor_p = sess.run(pmps.populatetensor())
-                    kl_p = np.sum(ptensor_p*(np.log(ptensor[copy])-np.log(ptensor_p)))
+                    kl_p = np.sum(ptensor_p*(np.log(ptensor_p)-np.log(ptensor[copy])))
                     df_p['ELBO'][(rank, copy)] = elbo_p
                     df_p['pred_llk'][(rank, copy)] = pred_p
                     df_p['KL'][(rank, copy)] = kl_p
@@ -156,11 +156,12 @@ with tf.name_scope("model"):
                 configc = config + (copy,)
                 opt[config].minimize(sess, feed_dict = {Xt[config]: X[copy]})
                 elbo_c, pred_c, qtensor_c = sess.run([trueelbo[config], pred[config], qtensor[config]], feed_dict = {Xt[config]: X[copy]})
-                kl_c = np.sum(qtensor_c*(np.log(ptensor[copy])-np.log(qtensor_c)))
+                kl_c = np.sum(qtensor_c*(np.log(qtensor_c)-np.log(ptensor[copy])))
                 df_c['ELBO'][configc] = elbo_c
                 df_c['pred_llk'][configc] = pred_c
                 df_c['KL'][configc] = kl_c
 
-supdict = {'df_c':df_c, 'df_p':df_p, 'logZ': logZ, }
+save_name = folder + config_full_name + '_optrank.pkl'
+supdict = {'name': save_name, 'df_c':df_c, 'df_p':df_p, 'logZ': logZ, 'X':X}
 with open(folder + config_full_name + '_optrank.pkl','wb') as handle:
     pickle.dump(supdict, handle, protocol=pickle.HIGHEST_PROTOCOL)

tensornets.py
@@ -33,7 +33,7 @@ def mosaic(Z, split=None):
         A[bitify(coord1, K), bitify(coord2, K)] = np.log(np.maximum(Z[ind], 0.))
     return A
 
-def full2TT(A, maxrank=np.inf):
+def full2TT(A, maxrank=np.inf, normalized=False):
     Z = A
     k = A.shape[0]
     ndim = A.ndim
@@ -53,7 +53,7 @@ def full2TT(A, maxrank=np.inf):
         rprev = r
     G[-1] *= Z
     cores = Core(ndim, k, ranks, cores=[tf.transpose(g,[1,0,2]) for g in G])
-    return cores, MPS(ndim, k, ranks, cores=cores, normalized=False)
+    return cores, MPS(ndim, k, ranks, cores=cores, normalized=normalized)
 
 def randomorthogonal(shape):
     '''Returns a random orthogonal matrix of shape Shape'''
@@ -277,8 +277,8 @@ class MPS:
                                                 tf.convert_to_tensor(1., dtype=dtype))
 
 
-        self._nuvar = tf.Variable(np.log(np.exp(1.)-1.), dtype=dtype)
-        self.nu = tf.nn.softplus(self._nuvar)
+        self._nuvar = tf.Variable(1., dtype=dtype)
+        self.nu = tf.identity(self._nuvar)
         if multi_temp:
             self._tempvar = tf.Variable(np.log(np.exp(0.5)-1.)*np.ones(N), dtype=dtype)
             self.temperatures = tf.nn.softplus(self._tempvar)
@@ -308,6 +308,9 @@ class MPS:
 
     def set_temperature(self, value):
         return tf.assign(self._tempvar, np.log(np.exp(value)-1.))
+
+    def set_nu(self, value):
+        return tf.assign(self._nuvar, value)
 
     @tfmethod(1)
     def contraction(self, Z, normalized=True):
@@ -845,6 +848,8 @@ class Canonical(Core):
         self.ranks = ranks
         self.right_canonical = not left
         self.left_canonical = left
+        self.initials = initial
+        self.orthogonalstyle=orthogonalstyle
 
 
 